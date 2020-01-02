f3c44d90f78a7a7df096b49c9712f47c01de33b1
Aryan Dixit <aryandixit1007@gmail.com>
2020-01-02 18:31:53 +0530

    Add files via upload

---
+++ Fund&Finance_Stat.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 20,10
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+df = pd.read_csv('AviationCargoIndex.csv')
+
+df.head()
+
+df['Date'] = pd.to_datetime(df.Date,format='%Y-%m-%d')
+df.index = df['Date']
+
+plt.figure(figsize=(16,8))
+plt.plot(df['Close'], label='Close Price history')
